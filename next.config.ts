4acf19e7e3e3f7ac9eaf47abd6eb166824fb0d9f
HeavieXo <heaviexo@HeavieXos-MacBook-Pro.local>
2026-07-28 15:17:30 +0100

    Limite upload 100mb v2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['better-sqlite3'],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
 };
 
 export default nextConfig;
