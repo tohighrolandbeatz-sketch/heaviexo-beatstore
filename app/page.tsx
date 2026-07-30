83a22d8163be2eacf43b7784a002f76317e17d50
HeavieXo <heaviexo@HeavieXos-MacBook-Pro.local>
2026-07-30 21:59:43 +0100

    fix: page d'accueil propre

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,7 @@
-export { default } from './beatstore/page';
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-black text-white tracking-wider uppercase animate-pulse">Welcome, please enter</h1>
+    </div>
+  );
+}
