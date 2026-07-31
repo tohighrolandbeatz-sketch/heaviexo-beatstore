7c873f805ce95ca1992101522bb6611ea5b4fe28
HeavieXo <heaviexo@HeavieXos-MacBook-Pro.local>
2026-07-31 01:10:56 +0100

    fix: ajout PHONE_OWNER_NAME

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,5 @@
 export const PHONE_WHATSAPP = "2290156646409";
+export const PHONE_OWNER_NAME = "GBOSSA TOLIDJI ROLAND GAEL";
 
 export const GENRES = ["All", "Dark Trap", "Melodic Drill", "Boom Bap", "Cinematic"];
 
